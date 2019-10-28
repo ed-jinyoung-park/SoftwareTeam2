a2a50280fa7bc6235181632dc8be0de391028271
Zinyon <jy2045@sogang.ac.kr>
2019-10-28 12:25:10 +0900

    Prototype Refactoring
    mysql DB 연동
    Create, read 기능 구현

--- team2/main.js
+++ team2/main.js
@@ -4,41 +4,27 @@ const express = require('express')
 const app = express();
 const port = 3000;
 var fs = require('fs');
-var template = require('./lib/template.js');
 var path = require('path');
 var qs = require('querystring');
 var bodyParser = require('body-parser');
 var compression = require('compression');
-var userRouter = require('./routes/user.js');
-// var sanitizeHTML = require('sanitize-html');
+var studentRouter = require('./routes/student.js');
+var ejsLocals = require('ejs-locals');
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.engine('ejs',ejsLocals);
 
-// Middle Ware
-// bodyParser - data 들어올때마다 자동으로 읽어줌
-app.use(bodyParser.urlencoded({ extended: false }));
-// data 압축
-app.use(compression())
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
-    request.list=filelist;
-    next();
-  })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// student create,show 등 모든 router
+app.use('/student', studentRouter);
+
+// main page
+app.get('/', function(req, res) { 
+  res.render('index');
 });
 
-app.use('/user', userRouter);
-
-app.get('/', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
-  var title = '취향에 맞는 시간표를 만들어드립니다';
-  var description = '';
-  var list = template.list(filelist);
-  var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description}`,
-    `<a href="/user/create">시작하기</a>`,
-    //authStatusUI(request, response)
-  );
-  response.send(html);
-  });
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ team2/routes/student.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var mysql = require('mysql');
+
+module.exports = router;
+
+// db connect
+var connection = mysql.createConnection ({
+  host:'localhost',
+  user:'root',
+  password:'0107',
+  database:'TEST'
+});
+
+connection.connect();
+
+
+// data create - 입력 화면으로 이동
+router.get('/create', function(req,res){
+  res.render('./student/create');
+});
+
+// data save - 입력한 데이터를 post방식으로 저장.
+router.post('/create',function(req,res){
+  console.log(req.body);
+
+  
+  // json 형식의 data를 user에 저장
+  var user = req.body;
+  var general_req = user.general_req;
+  console.log(general_req.length);
+  connection.query('INSERT INTO student (name, adm_year, semester, major1, major2, major3) VALUES(?, ?, ?, ?, ?, ?)', [user.user_name, user.user_number, user.semester, user.major1, user.major2, user.major3], function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+  });
+
+
+  var sql1 = 'INSERT INTO student_taken (major_required, major_select, general_required, general_select, id)';
+  var sql2 = '(SELECT id from student where name = ?)'
+  
+  var major_req= user.major_req;
+  var major_sel = user.major_sel;
+  var general_req = user.general_req;
+  var general_sel= user.general_sel;
+    
+  connection.query(sql1+'values (?, ?, ?, ?, '+sql2+')', [major_req.join(), major_sel.join(), general_req.join(), general_sel.join(), user.user_name], function (error, results, fields) {
+      if (error) {
+          console.log(error);
+      }
+      console.log(results);
+  });
+
+  sql3='INSERT INTO requirement (grade_range, major_req_num, major_sel_num, general_req_num, general_sel_num, blank_day, must_take, su_included, id)'
+  connection.query(sql3+ 'values(?, ?, ?, ?, ?, ?, ?, ?, '+sql2+')', [user.grade_range, Number(user.major_req_num), Number(user.major_sel_num), Number(user.general_req_num), Number(user.general_sel_num), user.blank_day, user.must_take, user.su_included, user.user_name], function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+  });
+
+  res.render('/show');
+});
+
+// data를 보여주는 read 화면
+router.get(['/show', '/show/:id'], function(req,res){
+  var sql_show = 'SELECT * from student';
+  connection.query(sql_show, function(error, results, fields ){
+    var id= req.params.id;
+    if (id){
+      var sql_show = 'SELECT s.*, r.*, st.* from student s, requirement r, student_taken st where s.id = ? and s.id=r.id and s.id=st.id';
+      connection.query(sql_show, [id],function(error, result, fields){
+        if (error) {
+          console.log(error);
+        } else{
+          console.log(result);
+          res.render('student/show', {students: result, student: result[0]});
+        }        
+      })
+    } else{
+      res.render('student/show', {students: results, student: undefined});
+    }
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+  })
+});
+// db connect end
