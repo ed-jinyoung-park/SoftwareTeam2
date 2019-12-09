da71529aa5d9414132a74f288c57bce2d0b72d59
Jin Young Park <jy2045@sogang.ac.kr>
2019-12-09 17:23:24 +0900

    추천로직 0단계

---
+++ team2/operations/SetTimetableScore.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var SVDJS = require('svd-js');
+var math = require('mathjs');
+var models = require('../models/index');
+
+// module.exports = setTimetableScore;
+
+module.exports = async function(studentId){
+  
+  var student = await models.student.findOne({
+    where: {id: studentId}
+  });
+  student = student.dataValues;
+  console.log(student);
+
+  var sameStudentList = await models.student.findAll({
+    where: {semester: student.semester, major1: student.major1, major2: student.major2, major3: student.major3}
+  });
+  sameStudentList = sameStudentList.map(student => student.id);
+  sameStudentList.splice(studentId-1,1);
+
+  var tt_list_all = await models.Timetable.findAll({
+    where: {studentId: sameStudentList},
+    attributes: ['id','studentId','title','day','start_time','end_time'],
+    raw: true
+  });
+
+  
+  var tt_matrix_list=[];
+  
+  for(i in sameStudentList){
+    var tt_list_one=[]; // student 1명의 시간표
+    var id = sameStudentList[i];
+    for(j in tt_list_all){
+      if(sameStudentList[i]==tt_list_all[j].studentId){
+        tt_list_one.push(tt_list_all[j]);
+      }
+    }
+
+    // student 1명의 시간표 matrix  
+    var tt_matrix = math.matrix([
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,0,0,0,0]
+    ]);
+
+    // 1개의 matrix 생성
+    for (k in tt_list_one){
+      tt_matrix = makeMatrix(tt_list_one[k], tt_matrix); 
+    }
+
+    var svd_q = math.sum(SVDJS.SVD(tt_matrix._data).q);
+    
+    var tt_matrix_json={id: id, matrix: tt_matrix._data, timetable: tt_list_one, score: svd_q}
+    // matrix List에 id 값과 함께 matrix를 push
+    tt_matrix_list.push(tt_matrix_json);
+  }
+
+  console.log(tt_matrix_list);
+
+  return tt_matrix_list;
+}
+
+
+// 수업 1개에 대해 matrix 만들기
+var makeMatrix= function(tt_list_one_class, tt_matrix){
+  
+  var day_list=changeDayToNum(tt_list_one_class.day);
+
+  var start_time = tt_list_one_class.start_time.trim();
+  var end_time = tt_list_one_class.end_time.trim();
+  start_time = parseInt(start_time)-1;
+  end_time = parseInt(end_time)-1;
+
+  // index(행,열),값
+  for(i in day_list){
+    if(start_time==end_time){
+      tt_matrix = math.subset(tt_matrix, math.index(start_time,day_list[i]),1);
+    }
+    else{
+      tt_matrix = math.subset(tt_matrix, math.index([start_time,end_time],day_list[i]),[1,1])
+    }
+  }
+  return tt_matrix;
+}
+
+// day data int로 변경
+var changeDayToNum= function(day){
+  var day_list=[];
+
+  if(day.includes(',')){
+    day_list=day.split(',');
+  }
+  else day_list=[day];
+  
+  for(i in day_list){
+    if (day_list[i]=='월') day_list[i]=0;
+    else if (day_list[i]=='화') day_list[i]=1;
+    else if (day_list[i]=='수') day_list[i]=2;
+    else if (day_list[i]=='목') day_list[i]=3;
+    else day_list[i]=4;
+  }
+
+  return day_list;
+}
---
+++ team2/operations/getfilteredTimetableList.js
@@ -0,0 +1,103 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var mysql = require('mysql');
+var models = require('../models/index');
+
+var aaa = async function(tt_matrix_list, studentId){
+  
+  // 고정 과목 리스트 추출 - sub_fix_list
+  var condition = await models.condition.findOne({
+    where: {studentId: studentId}
+  });
+  condition = condition.dataValues;
+  var sub_fix_list = [];
+  if (condition.sub_fix_1 != undefined){
+    sub_fix_list.push(condition.sub_fix_1);
+  }
+  if (condition.sub_fix_2 != undefined){
+    sub_fix_list.push(condition.sub_fix_2);
+  }
+  if(condition.sub_fix_3 != undefined){
+    sub_fix_list.push(condition.sub_fix_3);
+  }
+  console.log(sub_fix_list);
+  // 개설과목리스트(Opencourses)에서 고정과목 개설정보 찾기
+  var Opencourses = await models.Opencourse.findAll({
+    where: {subject_title: sub_fix_list},
+    attributes: ['subject_title','class_day','start_time','end_time'],
+    raw: true
+  });
+
+  console.log(Opencourses);
+  var sub_fix_timetable_list=[]
+  for (i in sub_fix_list){
+    sub_fix_timetable_json=[];
+    for(j in Opencourses){
+      if (Opencourses[j].subject_title==sub_fix_list[i]){
+        var sub_fix_timetable={class_day: Opencourses[j].class_day, start_time: Opencourses[j].start_time, end_time: Opencourses[j].end_time}
+        sub_fix_timetable_json.push(sub_fix_timetable);
+      }
+    }
+    var title =sub_fix_list[i];
+    sub_fix_timetable_with_title={title: title, timetable: sub_fix_timetable_json};
+    sub_fix_timetable_list.push(sub_fix_timetable_with_title);
+  }
+
+  console.log(sub_fix_timetable_list);
+
+  var tt_matrix_list_new=[];
+
+  for(i in tt_matrix_list){
+    var tt_matrix = tt_matrix_list[i].matrix;
+    for (j in sub_fix_timetable_list){
+      for (k in sub_fix_timetable_list[j].timetable){
+        matchTimetable(tt_matrix, sub_fix_timetable_list[j].timetable[k]); 
+      }
+    }
+  }
+
+}
+
+
+var matchTimetable = function(tt_matrix, time){
+
+  var day_list = changeDayToNum(time.class_day);
+  var start_time = time.start_time.trim();
+  var end_time = time.end_time.trim();
+  start_time = parseInt(start_time)-1;
+  end_time = parseInt(end_time)-1;
+
+  for(i in day_list){
+    if(start_time == end_time){
+      var a = tt_matrix.subset(math.index(start_time,day_list[i]));
+      if (!(math.equal(a,1))) return false;
+    }
+    else{
+      var a = tt_matrix.subset(math.index([start_time,end_time],day_list[i]));
+      if (!(math.deepEqual(a,[1,1]))) return false;
+    }
+  }
+}
+
+
+// day data int로 변경
+var changeDayToNum= function(day){
+  var day_list=[];
+
+  if(day.includes(',')){
+    day_list=day.split(',');
+  }
+  else day_list=[day];
+  
+  for(i in day_list){
+    if (day_list[i]=='월') day_list[i]=0;
+    else if (day_list[i]=='화') day_list[i]=1;
+    else if (day_list[i]=='수') day_list[i]=2;
+    else if (day_list[i]=='목') day_list[i]=3;
+    else day_list[i]=4;
+  }
+
+  return day_list;
+}
